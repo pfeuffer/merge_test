e51b85ce09b85bd951c2b466024bd736b55e18c1
Rene Pfeuffer <rene.pfeuffer@cloudogu.com>
2019-11-06 09:38:40 +0100

    Add explanation

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 class Main {
     public static void main(String[] args) {
         System.out.println("Expect nothing more to happen.");
+        System.out.println("This is for demonstration, only.");
     }
 }
