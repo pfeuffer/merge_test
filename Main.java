74b0423748644aa03254f0f0c9a147e0ab10b957
Rene Pfeuffer <rene.pfeuffer@cloudogu.com>
2019-11-06 09:45:47 +0100

    Print arguments

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
+import java.util.Arrays;
+
 class Main {
     public static void main(String[] args) {
         System.out.println("Expect nothing more to happen.");
+        System.out.println("Parameters:");
+        Arrays.stream(args).map(arg -> "- " + arg).forEach(System.out::println);
     }
 }
